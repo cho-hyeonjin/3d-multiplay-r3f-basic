ee68c8a85569ce877f01d29ecdde4fac28d63b99
cho-hyeonjin <gusls2005@gmail.com>
2024-04-23 16:10:29 +0900

    drei 의 Html 숏컷으로 Threejs canvas에서 html 표현하기

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,30 +1,45 @@
 /* eslint-disable react/no-unknown-property */
-import { useFrame } from "@react-three/fiber";
+import { Html, OrbitControls } from "@react-three/drei";
+// import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
 
 function Experience() {
   const cubeRef = useRef();
 
-  useFrame((state, delta) => {
-    cubeRef.current.rotation.y += delta;
-  });
+  // useFrame((state, delta) => {
+  //   cubeRef.current.rotation.y += delta;
+  // });
 
-  /** 카메라 꺼내기 */
-  useFrame((state, delta) => {
-    const angle = state.clock.elapsedTime;
+  // /** 카메라 꺼내기 */
+  // useFrame((state, delta) => {
+  //   const angle = state.clock.elapsedTime;
 
-    state.camera.position.x = Math.sin(angle) * 8;
-    state.camera.position.z = Math.cos(angle) * 8;
-    state.camera.lookAt(0, 0, 0);
-  });
+  //   state.camera.position.x = Math.sin(angle) * 8;
+  //   state.camera.position.z = Math.cos(angle) * 8;
+  //   state.camera.lookAt(0, 0, 0);
+  // });
 
   return (
     <>
+      <OrbitControls enableDamping={true} />
+
+      <Html
+        position={[0, 2, 0]}
+        wrapperClass="label"
+        center
+        distanceFactor={10}
+      >
+        Html이다..!
+      </Html>
+
       <ambientLight intensity={0.2} />
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
 
       <mesh position-x={-2}>
+        <Html occlude={[cubeRef]} wrapperClass="label" center>
+          동글동글
+        </Html>
         <sphereGeometry />
         <meshStandardMaterial color="#82e0aa" />
       </mesh>

8411157e0c8f08a8cbe8675f2db5b347a54e3ea6
cho-hyeonjin <gusls2005@gmail.com>
2024-04-24 16:18:06 +0900

    test for physics

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,6 +2,8 @@ import * as THREE from "three";
 import { OrbitControls } from "@react-three/drei";
 import { Physics, RigidBody } from "@react-three/rapier";
 import { useRef } from "react";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { useLoader } from "@react-three/fiber";
 export default function Experience() {
   const rb = useRef();
 
@@ -10,13 +12,15 @@ export default function Experience() {
     rb.current.applyImpulse({ x: 0, y: 20, z: 0 });
   };
 
+  const model = useLoader(GLTFLoader, "./books_with_magnifier.glb");
+
   return (
     <>
       <ambientLight intensity={0.5} />
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <OrbitControls makeDefault />
-      <Physics debug>
-        <RigidBody ref={rb} colliders="ball" position={[-1.5, 2, 0]}>
+      <Physics>
+        <RigidBody debug ref={rb} colliders="ball" position={[-1.5, 2, 0]}>
           <mesh castShadow onClick={jump}>
             <sphereGeometry />
             <meshStandardMaterial color="#82E0AA" />
@@ -33,6 +37,10 @@ export default function Experience() {
             <boxGeometry args={[10, 0.1, 10]} />
             <meshStandardMaterial color="#5D6D7E" />
           </mesh>
+        </RigidBody>
+
+        <RigidBody scale={3} position={[0, 2, 2]}>
+          <primitive object={model.scene} />
         </RigidBody>
       </Physics>
     </>

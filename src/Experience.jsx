9bc957fdaba4ab6679e3af1da1a38d83fb795fb2
cho-hyeonjin <gusls2005@gmail.com>
2024-04-24 16:08:35 +0900

    manipulating mesh with Physics of rapier library

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,23 @@
 import * as THREE from "three";
 import { OrbitControls } from "@react-three/drei";
 import { Physics, RigidBody } from "@react-three/rapier";
+import { useRef } from "react";
 export default function Experience() {
+  const rb = useRef();
+
+  const jump = () => {
+    console.log(rb);
+    rb.current.applyImpulse({ x: 0, y: 20, z: 0 });
+  };
+
   return (
     <>
       <ambientLight intensity={0.5} />
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <OrbitControls makeDefault />
       <Physics debug>
-        <RigidBody colliders="ball" position={[-1.5, 2, 0]}>
-          <mesh castShadow>
+        <RigidBody ref={rb} colliders="ball" position={[-1.5, 2, 0]}>
+          <mesh castShadow onClick={jump}>
             <sphereGeometry />
             <meshStandardMaterial color="#82E0AA" />
           </mesh>

870618298c3bf6eae91c9a02af33d7058432a2e6
cho-hyeonjin <gusls2005@gmail.com>
2024-04-24 11:51:32 +0900

    init for 3d model & animation part

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-/* eslint-disable react/no-unknown-property */
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import Experience from "./Experience";
-import * as THREE from "three";
-
-const onCreated = (state) => {
-  /** 배경 설정 방법 2 - renderer 이용하기 */
-  // const gl = state.gl;
-  // gl.setClearColor("#ff00ff", 1);
-  /** 배경 설정 방법 3 - scene으로 설정하기 */
-  // const scene = state.scene;
-  // scene.background = new THREE.Color("#00ff00");
-};
-
 function App() {
   return (
-    <>
-      <Canvas shadows onCreated={onCreated}>
-        {/* 배경 설정 방법 4 - attach attribute 사용하기 */}
-        {/* <color args={["yellow"]} attach="background" /> */}
-        <Experience></Experience>
-      </Canvas>
-    </>
+    <Canvas shadows>
+      <Experience />
+    </Canvas>
   );
 }
-
 export default App;
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,46 +1,16 @@
-/* eslint-disable react/no-unknown-property */
-import { Environment, OrbitControls } from "@react-three/drei";
-import { useRef } from "react";
-import { Perf } from "r3f-perf";
 import * as THREE from "three";
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 export default function Experience() {
-  const cubeRef = useRef();
-  const sphereRef = useRef();
-
   return (
     <>
-      {/* posx, posy, negx, negy, posz, negz로 이미지 붙이기 */}
-      {/* <Environment
-        background
-        files={[
-          "./Yokohama/posx.jpg",
-          "./Yokohama/negx.jpg",
-          "./Yokohama/posy.jpg",
-          "./Yokohama/negy.jpg",
-          "./Yokohama/posz.jpg",
-          "./Yokohama/negz.jpg",
-        ]}
-      /> */}
-      {/* 좀 더 선명한 이미지 hdri */}
-      {/* <Environment background files="./hdri/lake_pier_4k.hdr" /> */}
-      {/* 화질은 구리지만 Three.js에 내장된 프리셋도 있음 */}
-      {/* <Environment background preset="sunset" /> */}
-
       <Perf position="top-left" />
-      <OrbitControls enableDamping={false} />
-      {/* <ambientLight intensity={0.5} />
-      <directionalLight position={[1, 2, 3]} intensity={1.5} /> */}
-
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} castShadow />
+      <OrbitControls makeDefault />
       <group>
-        <mesh ref={sphereRef} position-x={-2}>
-          <sphereGeometry />
-          <meshStandardMaterial color="#82E0AA" />
-        </mesh>
-        <mesh ref={cubeRef} position-x={2} scale={1.5}>
-          <boxGeometry />
-          <meshStandardMaterial color="#F9E79F" />
-        </mesh>
         <mesh
+          receiveShadow
           position-y={-1}
           rotation-x={THREE.MathUtils.degToRad(-90)}
           scale={10}
